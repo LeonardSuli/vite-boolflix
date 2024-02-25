3cf1dfb9a415155196353a7e40b9ff324438c3ed
LeonardSuli <leonardsuli1991@gmail.com>
2024-02-25 12:33:36 +0100

    add hover on movies and series cards

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -46,24 +46,31 @@ export default{
                     
                     <div class="movie_card">    <!-- // v-if="movie.vote_average !== 0" -->
 
-                        <img :src="movie.poster_path ? `https://image.tmdb.org/t/p/w200${movie.poster_path}` : '/images.png'" alt="">
-                        <div>Titolo: {{ movie.title }}</div>
-                        <div>Titolo originale: {{ movie.original_title }}</div>
-                        <div>Lingua: {{ movie.original_language }}</div>
-                        <div>Lingua: <img style="width: 20px;" :src="`/flags/${movie.original_language === 'en' ? 'gb' : movie.original_language }.svg`" alt="Bandierina stato"></div>
-                        <div>Voto: {{ movie.vote_average }}</div>
+                        <img :src="movie.poster_path ? `https://image.tmdb.org/t/p/w200${movie.poster_path}` : '/image.jpg'" alt="">
 
-                        <div class="star_vote_average">
+                        <div class="information">
 
-                            <!-- Calcolo del numero di stelle piene -->
-                            <span v-for="i in Math.ceil(movie.vote_average / 2)">
-                                <i class="fa-solid fa-star"></i>
-                            </span>
+                            <div>Titolo: {{ movie.title }}</div>
+                            <div>Titolo originale: {{ movie.original_title }}</div>
+                            <div>Lingua: {{ movie.original_language }}</div>
+                            <div>Lingua: <img style="width: 20px;" :src="`/flags/${movie.original_language === 'en' ? 'gb' : movie.original_language }.svg`" alt="Bandierina stato"></div>
+                            <div>Voto: {{ movie.vote_average }}</div>
     
-                            <!-- Calcolo del numero di stelle vuote -->
-                            <span v-for="i in (5 - Math.ceil(movie.vote_average / 2))">
-                                <i class="fa-regular fa-star"></i>
-                            </span>
+                            <div class="star_vote_average">
+    
+                                <span>Voto:</span>
+    
+                                <!-- Calcolo del numero di stelle piene -->
+                                <span v-for="i in Math.ceil(movie.vote_average / 2)">
+                                    <i class="fa-solid fa-star"></i>
+                                </span>
+        
+                                <!-- Calcolo del numero di stelle vuote -->
+                                <span v-for="i in (5 - Math.ceil(movie.vote_average / 2))">
+                                    <i class="fa-regular fa-star"></i>
+                                </span>
+    
+                            </div>
 
                         </div>
 
@@ -76,24 +83,29 @@ export default{
                         
                     <div class="serieTV_card">        <!-- v-if="serie.vote_average !== 0" -->
                             
-                        <img :src="`https://image.tmdb.org/t/p/w200${serie.poster_path}`" alt="">-
-                        <div>Nome: {{ serie.name }}</div>
-                        <div>Nome originale: {{ serie.original_name }}</div>
-                        <div>Lingua: {{ serie.original_language }}</div>
-                        <div>Lingua: <img style="width: 20px;" :src="`/flags/${serie.original_language === 'en' ? 'gb' : serie.original_language }.svg`" alt="Bandierina stato"></div>
-                        <div>Voto: {{ serie.vote_average }}</div>
+                        <img :src="serie.poster_path ? `https://image.tmdb.org/t/p/w200${serie.poster_path}` : '/image.jpg'" alt="">
 
-                        <div class="star_vote_average">
+                        <div class="information">
 
-                            <!-- Calcolo del numero di stelle piene -->
-                            <span v-for="i in Math.ceil(serie.vote_average / 2)">
-                                <i class="fa-solid fa-star"></i>
-                            </span>
+                            <div>Nome: {{ serie.name }}</div>
+                            <div>Nome originale: {{ serie.original_name }}</div>
+                            <div>Lingua: {{ serie.original_language }}</div>
+                            <div>Lingua: <img style="width: 20px;" :src="`/flags/${serie.original_language === 'en' ? 'gb' : serie.original_language }.svg`" alt="Bandierina stato"></div>
+                            <div>Voto: {{ serie.vote_average }}</div>
     
-                            <!-- Calcolo del numero di stelle vuote -->
-                            <span v-for="i in (5 - Math.ceil(serie.vote_average / 2))">
-                                <i class="fa-regular fa-star"></i>
-                            </span>
+                            <div class="star_vote_average">
+    
+                                <!-- Calcolo del numero di stelle piene -->
+                                <span v-for="i in Math.ceil(serie.vote_average / 2)">
+                                    <i class="fa-solid fa-star"></i>
+                                </span>
+        
+                                <!-- Calcolo del numero di stelle vuote -->
+                                <span v-for="i in (5 - Math.ceil(serie.vote_average / 2))">
+                                    <i class="fa-regular fa-star"></i>
+                                </span>
+    
+                            </div>
 
                         </div>
 
@@ -117,5 +129,24 @@ export default{
 
 .movie_card, .serieTV_card{
     margin: 1rem;
+}
+
+.information{
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: none;
+    width: 100%;
+    height: 100%;
+}
+
+.movie_card, .serieTV_card{
+    position: relative;
+}
+
+.movie_card:hover .information, 
+.serieTV_card:hover .information{
+    display: block;
+    background-color: black;
 }
 </style>
